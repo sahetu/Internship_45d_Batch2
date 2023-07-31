369c3faa9fd1b9fbb4637477f4524d2efff57f3f
Sagar Khatri <khatrisagar2@gmail.com>
2023-07-31 11:58:44 +0530

    31st July Updated Code

--- app/src/main/java/internship/batch2/MainActivity.java
+++ app/src/main/java/internship/batch2/MainActivity.java
@@ -3,13 +3,40 @@ package internship.batch2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button login;
+    EditText email, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("Hello Everyone");
+
+        login = findViewById(R.id.main_login);
+        email = findViewById(R.id.main_email);
+        password = findViewById(R.id.main_password);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (email.getText().toString().trim().equals("")) {
+                    email.setError("Email Id Required");
+                } else if (password.getText().toString().trim().equals("")) {
+                    password.setError("Password Required");
+                } else {
+                    System.out.println("Login Successfully\nEmail:" + email.getText().toString() + "\nPassword:" + password.getText().toString());
+                    Toast.makeText(MainActivity.this, "Login Successfully", Toast.LENGTH_LONG).show();
+                    Snackbar.make(view, "Login Successfully", Snackbar.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
